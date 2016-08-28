4d2ef5d5d9aa8ab9d12de2a24d5b3fb0cb509685
Jimmy Hodgson <Jimmy Hodgson>
2016-08-27 22:14:26 -0600

    fixes en estudiante Lista de carreras como parametro por defecto persistia entre objetos y no generaba una nueva lista al crear un objeto. Se agregaron más test cases para la clase estudiante.

--- bin/estudiante.py
+++ bin/estudiante.py
@@ -1,22 +1,24 @@
 from persona import Persona
 class Estudiante(Persona):
-    def __init__(self,nombre,edad,correo,carreras=[]):
+    def __init__(self,nombre,edad,correo,carreras=None):
         Persona.__init__(self,nombre,edad,correo)
+        if(carreras is None):
+            carreras=[]
         self.__carreras = carreras
 
     def getCarreras(self):
         return self.__carreras
 
     def addCarrera(self,carrera):
-        if(len(self.__carreras)<2):
+        if(len(self.__carreras)<2 and self.__carreras.count(carrera)==0):
             self.__carreras.append(carrera)
             return 0
         else:
             return 1
 
     def removeCarrera(self,carrera):
-        if(len(self.__carreras)>0&self.__carreras.count(carrera)>0):
-            self.__carreras.remove(self.__carreras.index(carrera));
+        if(len(self.__carreras)>0 and self.__carreras.count(carrera)>0):
+            self.__carreras.remove(carrera);
             return 0
         else:
             return 1
--- test/estudianteTest.py
+++ test/estudianteTest.py
@@ -11,4 +11,25 @@ class EstudianteTest(unittest.TestCase):
         estudiante1 = Estudiante('Prueba',22,'prueba@ejemplo.com')
         self.assertEqual(estudiante1.getCarreras(),[])
         estudiante1.addCarrera('Literatura')
-        self.assertEqual(estudiante1.getCarreras(),['Literatura'])
+        self.assertEqual(estudiante1.getCarreras(),['Literatura'])  
+    def testSetCarreraDuplicate(self):
+        estudiante1 = Estudiante('Prueba',22,'prueba@ejemplo.com')
+        self.assertEqual(estudiante1.addCarrera('Sistemas'),0)
+        self.assertEqual(estudiante1.addCarrera('Sistemas'),1)
+        self.assertEqual(estudiante1.getCarreras(),['Sistemas'])
+    def testSetCarreraMax(self):
+        estudiante1 = Estudiante('Prueba',22,'prueba@ejemplo.com')
+        self.assertEqual(estudiante1.getCarreras(),[])
+        self.assertEqual(estudiante1.addCarrera('Sistemas'),0)
+        self.assertEqual(estudiante1.addCarrera('Literatura'),0)
+        self.assertEqual(estudiante1.addCarrera('Derecho'),1)
+        self.assertEqual(estudiante1.getCarreras(),['Sistemas','Literatura'])
+    def testRemoveCarrera(self):
+        estudiante1 = Estudiante('Prueba',22,'prueba@ejemplo.com',['Sistemas'])
+        self.assertEqual(estudiante1.getCarreras(),['Sistemas'])
+        self.assertEqual(estudiante1.removeCarrera('Sistemas'),0)
+        self.assertEqual(estudiante1.getCarreras(),[])
+        self.assertEqual(estudiante1.removeCarrera('Sistemas'),1)
+        self.assertEqual(estudiante1.getCarreras(),[])
+        
+        
